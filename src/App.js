0af570565aed8c3b3fe5074351ca575da7328e31
Eric Cerio <eric.cerio@appetiser.com.au>
2025-08-09 09:04:57 +0800

    Update with react and tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Portfolio from './eric-portfolio';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
